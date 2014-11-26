b3eb021376565803e4fb7e2f7ea00cf5d2612ac7
Fabien Raulline <fraulline@gmail.com>
2014-11-26 14:09:55 +0100

    Création premiere scene test

---
+++ Machinae/Assets/Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveHorizontal = Input.GetAxis("Horizontal");
+    public float moveVertical = Input.GetAxis("Vertical");
+    public float speed;
+
+	// Update is called once per frame
+	void Update () 
+    {
+	       
+	}
+}

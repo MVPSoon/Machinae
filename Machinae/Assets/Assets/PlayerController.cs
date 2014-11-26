252eb6aea4f1f1f2a4974912d482af7dc202aa85
Fabien Raulline <fraulline@gmail.com>
2014-11-26 14:17:03 +0100

    Correction speed deplacement test

--- Machinae/Assets/Assets/PlayerController.cs
+++ Machinae/Assets/Assets/PlayerController.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveHorizontal = Input.GetAxis("Horizontal");
-    public float moveVertical = Input.GetAxis("Vertical");
+    
     public float speed;
 
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
     {
-	       
+	       float moveHorizontal = Input.GetAxis("Horizontal");
+           float moveVertical = Input.GetAxis("Vertical");
+
+           Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+           rigidbody.AddForce(movement * speed * Time.deltaTime);
 	}
 }

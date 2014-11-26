8b1e0cc5487c28ef5641430c69629ae9dd5a51b3
Fabien Raulline <fraulline@gmail.com>
2014-11-26 14:37:57 +0100

    Oubli premiere scene + script camera

---
+++ Machinae/Assets/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour 
+{
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 position = new Vector3(0.0f, 10f, -15f);
+        transform.position = Player.transform.position + position;
+	}
+}

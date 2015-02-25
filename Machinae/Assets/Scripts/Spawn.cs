862b2c6d510ca05eb9b533a1aa966390c60da725
demorite <dylan3396@hotmail.fr>
2015-02-25 12:00:56 +0100

    Mini spawn script commenté

--- Machinae/Assets/Scripts/Spawn.cs
+++ Machinae/Assets/Scripts/Spawn.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class Spawn3 : MonoBehaviour {
 
-    public GameObject mobs;
-    public float spawnTime = 2f;
-    public Transform spawnPoint;
-    public int range;
+    public GameObject mobs; // Ennemi à invoquer 
+    public float spawnTime = 2f; // Temps entre chaque instance
+    public Transform spawnPoint; // Point autour duquel les ennemies vont etre instanciés
+    public int range; // Rayon d'instance autour du point de spawn
 
 
     // Use this for initialization
     void Start()
     {
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        InvokeRepeating("Spawn", spawnTime, spawnTime); // répétition du spawn d'ennemis
     }
 
     void Spawn()
@@ -25,7 +25,7 @@ public class Spawn3 : MonoBehaviour {
         z = spawnPoint.position.z + Random.Range(-range, range);
 
 
-        Instantiate(mobs, new Vector3(x,spawnPoint.position.y,z), spawnPoint.rotation);
+        Instantiate(mobs, new Vector3(x,spawnPoint.position.y,z), spawnPoint.rotation); // Création d'un ennemi
     }
 
 

41329dde98e0f2c1caa1160b9412f54c877fb541
Emmanuel Barreau <barreau.emmanuel@gmail.com>
2015-02-25 15:25:41 +0100

    Basic main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public void BeginGame()
+    {
+        Application.LoadLevel("main_scene");
+    }
+    public void ContinueGame()
+    {
+        //TODO A voir quand Romain aura fini de faire les fonctions de sauvegarde et de chargement.
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

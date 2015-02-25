b53d66fb3d6d862e02a2cfbf1bb55b96e3ca14b7
Romain Poussin <romain.poussin@y-nov.com>
2015-02-25 15:15:39 +0100

    Save&Load PlayerData

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -4,32 +4,48 @@ using UnityEngine.UI;
 
 public class PlayerData : MonoBehaviour 
 {
-
     public Text niveau;
     public Text experience;
     public Text sante;
 
-    public float timeInterval = 1f;
+    public float timeInterval;
 
-    private float exp = 1;
-    private int level = 0;
-    private int pointCompetence = 0;
+    private float exp;
+    private int level;
+    private int pointCompetence;
 
-    private int vie = 100;
-    private int att = 10;
-    private int def = 10;
+    private int vie;
+    private int att;
+    private int def;
 
     private int currentlife;
+    public void Constructor()
+    {
+        timeInterval = 1f;
+        exp = 1f;
+        level = 1;
+        pointCompetence = 0;
+        vie = 100;
+        att = 10;
+        def = 10;
+        currentlife = vie;
+    }
+    
 
     void Awake()
     {
         LevelCap.LoadCap();
-        currentlife = vie;
+        if (PlayerPrefs.HasKey("PositionX"))
+        {
+            SaveGameData.Load();
+        }
+        else Constructor();
     }
     void Update()
     {
+        niveau.text = "Niveau: " + level;
         sante.text = "SANTE: " + currentlife + "/" + vie;
-        if (level < 4)
+        if (level < 3)
         {
             exp++;
             experience.text = "Exp: " + (int)exp + "/" + (int)LevelCap.GetExpLevel(level);
@@ -39,27 +55,79 @@ public class PlayerData : MonoBehaviour
                 pointCompetence++;
                 exp = 0;
             }
-            niveau.text = "Niveau: " + (level+1);
-
+        }
+        else
+        {
+            SetCurrentLife(0);
+            SaveGameData.Save();
         }
     }
 	
+    public float GetExp ()
+    {
+        return exp;
+    }
+
+    public void SetExp(float experience)
+    {
+        exp = experience;
+    }
+
+    public int GetLevel ()
+    {
+        return level;
+    }
+
+    public void SetLevel (int lvl)
+    {
+        level = lvl;
+        Debug.Log("this.level : " + this.level);
+    }
     public int GetVie()
     {
         return vie;
+    }
+
+    public void SetVie (int Life)
+    {
+        vie = Life;
     }
 
     public int GetAtt()
     {
         return att;
     }
+
+    public void SetAtt (int Atk)
+    {
+        att = Atk;
+    }
     public int GetDef()
     {
         return def;
     }
+
+    public void SetDef (int defense)
+    {
+        def = defense;
+    }
     public int GetPt()
     {
         return pointCompetence;
+    }
+
+    public void SetPt(int SkillPoint)
+    {
+        pointCompetence = SkillPoint;
+    }
+    public int GetCurrentLife ()
+    {
+        return currentlife;
+    }
+
+    public void SetCurrentLife (int CurentLife)
+    {
+        currentlife = CurentLife;
     }
     public void AddLife()
     {
@@ -79,5 +147,4 @@ public class PlayerData : MonoBehaviour
         def += 1;
         pointCompetence--;
     }
-
 }
---
+++ Assets/Scripts/SaveGameData.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveGameData : MonoBehaviour
+{
+    private static GameObject player = GameObject.FindGameObjectWithTag("Player"); 
+    private static PlayerData PlayerD = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerData>();
+    public static void Save ()
+    {
+        PlayerPrefs.SetFloat("PositionX", player.transform.position.x);
+        PlayerPrefs.SetFloat("PositionY", player.transform.position.y);
+        PlayerPrefs.SetFloat("PositionZ", player.transform.position.z);
+        PlayerPrefs.SetFloat("Experience", PlayerD.GetExp());
+        PlayerPrefs.SetInt("Level", PlayerD.GetLevel());
+        PlayerPrefs.SetInt("SkillPoints", PlayerD.GetPt());
+        PlayerPrefs.SetInt("Life", PlayerD.GetVie());
+        PlayerPrefs.SetInt("Attack", PlayerD.GetAtt());
+        PlayerPrefs.SetInt("Defense", PlayerD.GetDef());
+        PlayerPrefs.SetInt("CurrentLife", PlayerD.GetCurrentLife());
+        PlayerPrefs.Save();
+    }
+
+    public static void Load ()
+    {
+        try
+        {
+            player.transform.position = new Vector3(PlayerPrefs.GetFloat("PositionX"), PlayerPrefs.GetFloat("PositionY"), PlayerPrefs.GetFloat("PositionZ"));
+            
+            PlayerD.SetExp(PlayerPrefs.GetFloat("Experience"));
+            PlayerD.SetLevel(PlayerPrefs.GetInt("Level"));
+            PlayerD.SetPt(PlayerPrefs.GetInt("SkillPoints"));
+            PlayerD.SetVie(PlayerPrefs.GetInt("Life"));
+            PlayerD.SetAtt(PlayerPrefs.GetInt("Attack"));
+            PlayerD.SetDef(PlayerPrefs.GetInt("Defense"));
+            PlayerD.SetCurrentLife(PlayerPrefs.GetInt("CurrentLife"));
+        }
+        catch (PlayerPrefsException PrefExcept)
+        {
+
+        }
+        
+    }
+}
